532ef3de0fc839eed748a1452b456df0f160e77a
walotta <walotta@126.com>
2022-01-16 22:11:13 +0800

    add --enable-preview

--- build.bash
+++ build.bash
@@ -1,4 +1,4 @@
 set ff=UNIX
 set -e
 mkdir runContain
-find ./src -name "*.java" -print0|xargs -0 javac -d runContain -cp ./lib/*.jar
+find ./src -name "*.java" -print0|xargs -0 javac -d runContain -cp ./lib/*.jar --enable-preview
