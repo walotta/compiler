7c9b736dba5741236ddf46d470a55a613215d823
walotta <walotta@126.com>
2022-01-16 22:16:21 +0800

    add --enable-preview

--- build.bash
+++ build.bash
@@ -1,3 +1,3 @@
 set ff=UNIX
 set -e
-find ./src -name "*.java" -print0|xargs -0 javac -d runContain -cp ./lib/*.jar --enable-preview
+find ./src -name "*.java" -print0|xargs -0 javac -d runContain -cp ./lib/*.jar --enable-preview --release 17
