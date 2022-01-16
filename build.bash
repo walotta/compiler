f5bf59474b31b2a0ae11e866038211979e19418b
walotta <walotta@126.com>
2022-01-16 22:18:26 +0800

    change to low version java

--- build.bash
+++ build.bash
@@ -1,3 +1,4 @@
 set ff=UNIX
 set -e
-find ./src -name "*.java" -print0|xargs -0 javac -d runContain -cp ./lib/*.jar --enable-preview --release 17
+mkdir runContain
+find ./src -name "*.java" -print0|xargs -0 javac -d runContain -cp ./lib/*.jar
--- src/MIR/IRMember.java
+++ src/MIR/IRMember.java
@@ -2,7 +2,16 @@ package MIR;
 
 import MIR.IRtype.IRBaseType;
 
-public record IRMember(IRBaseType memberType, int memberCnt) {
+import java.util.Objects;
+
+public final class IRMember {
+    private final IRBaseType memberType;
+    private final int memberCnt;
+
+    public IRMember(IRBaseType memberType, int memberCnt) {
+        this.memberType = memberType;
+        this.memberCnt = memberCnt;
+    }
 
     public int size() {
         return memberType.size();
@@ -12,4 +21,27 @@ public record IRMember(IRBaseType memberType, int memberCnt) {
     public String toString() {
         return memberType.toString();
     }
+
+    public IRBaseType memberType() {
+        return memberType;
+    }
+
+    public int memberCnt() {
+        return memberCnt;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (IRMember) obj;
+        return Objects.equals(this.memberType, that.memberType) &&
+                this.memberCnt == that.memberCnt;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(memberType, memberCnt);
+    }
+
 }
